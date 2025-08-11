045646344f5c97b0ac4030294f39fb14c43e9eb6
mabq <alejandro.banderas@me.com>
2025-08-11 16:18:52 -0500

    all

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,15 +1,15 @@
 import React from "react";
 
 function Banner({ status, resetGame, children }) {
-  const className = status ? `banner ${status}` : "banner";
-  return (
-    <div className={className}>
-      {children}
-      <button className="reset-button" onClick={resetGame}>
-        Reset game
-      </button>
-    </div>
-  );
+	const className = status ? `banner ${status}` : "banner";
+	return (
+		<div className={className}>
+			{children}
+			<button className="reset-button" onClick={resetGame}>
+				Reset game
+			</button>
+		</div>
+	);
 }
 
 export default Banner;
